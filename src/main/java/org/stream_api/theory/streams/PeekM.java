6510a051b10c69d42d46db7cf83e1e6ffd6aa3a7
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-01-25 17:57:58 +0300

    stream peek() add

--- src/main/java/org/stream_api/theory/streams/PeekM.java
+++ src/main/java/org/stream_api/theory/streams/PeekM.java
@@ -1,19 +1,30 @@
 package org.stream_api.theory.streams;
 
-/** PEEK
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * PEEK
  * Метод peek(), так же как и forEach() принимает в параметр Consumer FI, но возвращает Stream
- *
+ * <p>
  * Чаще всего он используется, для просмотра этапов method chaining для отладки
- *
+ * <p>
  * peek	- выполнить действие над каждым элементом элементов, вернет стрим с исходными элементами
  * > collection.stream()
- *  .map(String::toLowerCase).peek((e) -> System.out.print("," + e))
- *  .collect(Collectors.toList())
+ * .map(String::toLowerCase).peek((e) -> System.out.print("," + e))
+ * .collect(Collectors.toList())
  */
 
 public class PeekM {
 
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 4, 3, 5, 6, 2, 5, 1};
+        Arrays.stream(arr).distinct()
+                .peek(e -> System.out.println(e + "Уникальный элемент"))
+                .filter(i -> i % 2 == 0)
+                .peek(e -> System.out.println(e + "Чётный элемент"))
+                .map(i -> i / 2)
+                .peek(e -> System.out.println(e + "Элемент поделён на 2"))
+                .forEach(System.out::println);
     }
 }
